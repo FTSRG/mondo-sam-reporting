d41bfb22efaf8d949f1533ad19945243f53f7cf0
Gabor Szarnyas <szarnyasg@gmail.com>
2015-12-18 19:29:02 +0100

    Changed colors in brewer

--- source_code.R
+++ source_code.R
@@ -1,6 +1,12 @@
 library(ggplot2)
 library(plyr)
 library(gdata)
+library(RColorBrewer)
+
+brewer.cols = brewer.pal(n = 4, name = "Spectral")
+brewer.cols = c("#D7191C", "#FDAE61", "#2B83BA", "#ABDDA4")
+#brewer.cols = c("#D7191C", "#ABDDA4", "#2B83BA", "#FDAE61")
+
 
 df = read.csv("ls_codemodel.csv")
 df$Value = df$Value / 10^9
@@ -26,5 +32,5 @@ ggplot(data=df, aes(x=Tool, y=Value, fill=Phase)) +
     theme_bw() +
     theme(legend.key=element_blank(), legend.title=element_blank(), legend.position="bottom", axis.text.x=element_text(angle=45, hjust=1)) +
     labs(title="Total execution time on source code models", x="Algorithm", y="Execution time [s]") +
-    scale_fill_brewer(palette="Spectral")
+    scale_fill_manual(values=brewer.cols)
 ggsave("diagrams/code_model.pdf", width=210, height=297, units="mm")
